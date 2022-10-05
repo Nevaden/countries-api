950900cd9dfd5b74ae286179d06f01d5495f8cbc
Nevaden <aaron.k.dodson@gmail.com>
2022-10-04 22:02:43 -0400

    header and input search bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailComponent } from './components/detail/detail.component';
+import { IndexComponent } from './components/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: "/home", pathMatch: "full"},
+  {path: 'home', component: IndexComponent},
+  {path: 'about', component: DetailComponent},
+  {path: '*', component: IndexComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './components/index/index.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { HeaderComponent } from './components/shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    DetailComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
